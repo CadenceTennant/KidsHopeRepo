45cfb372b366f4455b641ae5af5308367ba0d91a
Cadence <cadence.tennant@hope.edu>
2023-04-06 11:10:57 -0400

    First real commit

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { axios } from 'axios';
+
 
 export default function App() {
+  let x = 1;
+  console.log("App executed");
+//  const fetchData = () => {
+//    const baseURL = "https://logindevsite.wpengine.com/wp-json/mo/v1/get_posts";
+//    axios.get(`${baseURL}`).then((response) => console.log(response.data));
+//  };
+//  useEffect(() => {
+//    fetchData();
+//  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Haniah!!</Text>
+      //{fetchData} this should show
       <StatusBar style="auto" />
     </View>
   );
